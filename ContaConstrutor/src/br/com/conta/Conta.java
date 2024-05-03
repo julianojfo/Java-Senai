f1b31bb7ee3b658ec874558c993730110c06cfe8
julianojfo <160608534+julianojfo@users.noreply.github.com>
2024-05-02 21:40:36 -0300

    Add files via upload

---
+++ ContaConstrutor/src/br/com/conta/Conta.java
@@ -0,0 +1,14 @@
+package br.com.conta;
+
+public class Conta {
+	//declaração de variáveis
+	String titular;
+	int numero;
+	double saldo;
+	
+	//construtor
+	Conta(String titular){
+		this.titular = titular;
+	}
+
+}
